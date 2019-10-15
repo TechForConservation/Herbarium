ec31b19ee92db20a5e026576e49f44459b79e92a
zacsimile <zachcm@gmail.com>
2019-10-15 19:43:23 -0400

    Download arbitrary number of images

--- image_scraper.py
+++ image_scraper.py
@@ -6,13 +6,14 @@ from os import path
 
 fn = '/Users/zachcm/Downloads/NEVP_phenology_1.0_scored_20190910.csv'
 destination = '/Users/zachcm/Downloads/NEVP_images/'
+n_images_to_download = 100
 
 data = pandas.DataFrame.from_csv(fn)
 
 if not os.path.exists(destination):
     os.mkdir(destination)
 
-for url in data['originalurl'][0:10]:
+for url in data['originalurl'][0:n_images_to_download]:
     print(url)
     r = requests.get(url, stream=True)
     path = "{}{}.jpg".format(destination, url.split('/')[-1])
