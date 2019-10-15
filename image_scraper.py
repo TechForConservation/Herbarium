d1b37dfd54c8900e3b03ca6c0842cc448b73ad6a
zacsimile <zachcm@gmail.com>
2019-10-15 19:37:59 -0400

    Grab the images from the CSV Nelson shared with us

---
+++ image_scraper.py
@@ -0,0 +1,21 @@
+import pandas 
+import requests
+import shutil
+import os
+from os import path
+
+fn = '/Users/zachcm/Downloads/NEVP_phenology_1.0_scored_20190910.csv'
+destination = '/Users/zachcm/Downloads/NEVP_images/'
+
+data = pandas.DataFrame.from_csv(fn)
+
+if not os.path.exists(destination):
+    os.mkdir(destination)
+
+for url in data['originalurl'][0:10]:
+    print(url)
+    r = requests.get(url, stream=True)
+    path = "{}{}.jpg".format(destination, url.split('/')[-1])
+    with open(path, 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
